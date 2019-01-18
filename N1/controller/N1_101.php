c1b76c7498d19deb5e305343530d0d6ad4e9e086
top <kritkrai@imake.co.th>
2019-01-18 23:13:54 +0700

    ggg
    gggg

---
+++ N1/controller/N1_101.php
@@ -0,0 +1,116 @@
+<?php
+ob_start();
+session_start();
+require '../../session.php';
+require_once '../../common.php';
+require_once '../../lib/template.php';
+require_once '../../config/database.php';
+require_once '../model/N1_101_Model.php';
+include '../../utils/base_function.php';
+
+//prepare template
+$template = new template();
+$template->set_filenames(array(
+    'body' => '../view/N1_101.html')
+);
+
+//get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+
+// prepare product object
+$model = new N1_101_Model($db);
+
+    $model->id = $_SESSION["id"];
+
+if (isset($_POST["search"])) {
+
+    $model->req_no = isNotEmpty($_POST["req_no"]);
+    $model->dept_name = isNotEmpty($_POST["dept_name"]);
+    $model->emp_name = isNotEmpty($_POST["emp_name"]);
+    $model->sdate = strToDateSdate(isNotEmpty($_POST["sdate"]));
+    $model->edate = strToDateEdate(isNotEmpty($_POST["edate"]));
+    $model->req_status_list = $_POST["req_status"];
+
+    $stmt = $model->search();
+
+    $filter_req_status = implode(",", $model->req_status_list);
+
+    if (is_array($model->req_status_list)) {
+        foreach ($model->req_status_list as $item) {
+            if ($item == 0) {
+                $template->assign_var("checked_0", "checked=''");
+            } elseif ($item == 1) {
+                $template->assign_var("checked_1", "checked=''");
+            } elseif ($item == 2) {
+                $template->assign_var("checked_2", "checked=''");
+            } elseif ($item == 3) {
+                $template->assign_var("checked_3", "checked=''");
+            }
+        }
+    }
+
+    $rpt_url = RPT_SERVER_ADDRESS;
+    $rpt_url = str_replace("{reportUnit}", "/reports/project/page1", $rpt_url);
+    $rpt_url = str_replace("{req_no}", isNotEmpty($_POST["req_no"]), $rpt_url);
+    $rpt_url = str_replace("{id}", isNotEmpty($_SESSION["id"]), $rpt_url);
+    $rpt_url = str_replace("{sdate}", str_replace("-", "/", isNotEmpty($_POST["sdate"])), $rpt_url);
+    $rpt_url = str_replace("{edate}", str_replace("-", "/", isNotEmpty($_POST["edate"])), $rpt_url);
+    $rpt_url = str_replace("{emp_name}", isNotEmpty($_POST["emp_name"]), $rpt_url);
+    $rpt_url = str_replace("{dept_name}", isNotEmpty($_POST["dept_name"]), $rpt_url);
+    $rpt_url = str_replace("{req_status}", $filter_req_status, $rpt_url);
+
+    $template->assign_var("dept_name", isNotEmpty($_POST["dept_name"]));
+    $template->assign_var("req_no", isNotEmpty($_POST["req_no"]));
+    $template->assign_var("emp_name", isNotEmpty($_POST["emp_name"]));
+    $template->assign_var("sdate", isNotEmpty($_POST["sdate"]));
+    $template->assign_var("edate", isNotEmpty($_POST["edate"]));
+
+    $template->assign_var("rpt_url", $rpt_url);
+
+} else {
+
+    $rpt_url = RPT_SERVER_ADDRESS;
+    $rpt_url = str_replace("{reportUnit}", "/reports/project/page1", $rpt_url);
+    $rpt_url = str_replace("{req_no}", "",$rpt_url);
+    $rpt_url = str_replace("{sdate}", "",$rpt_url);
+    $rpt_url = str_replace("{id}", isNotEmpty($_SESSION["id"]), $rpt_url);
+    $rpt_url = str_replace("{edate}", "",$rpt_url);
+    $rpt_url = str_replace("{emp_name}", "",$rpt_url);
+    $rpt_url = str_replace("{dept_name}", "", $rpt_url);
+    $rpt_url = str_replace("{req_status}", "0" ,$rpt_url);
+    
+    $template->assign_var("rpt_url", $rpt_url);
+    $template->assign_var("checked_0", "checked=''");
+    $stmt = $model->getAll();
+}
+
+$template->assign_var("xlsx_url", str_replace("output=pdf", "output=xlsxNoPag", $rpt_url));
+
+$num = $stmt->rowCount();
+if ($num > 0) {
+    $response_data = array();
+    $count = 1;
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $row["no"] = $count;
+        $row["adminstatus"] = reformatStatusResign($row["adminstatus"]);
+        $row["activities_enddate"] = date("Y-m-d H:i", strtotime($row["activities_enddate"]));
+        $template->assign_block_vars('request', $row);
+        unset($rows);
+        $count++;
+    }
+}
+
+$data = array(
+    "username" => $_SESSION["username"],
+    "type"     => $_SESSION["type"],
+    "pre_name" => $_SESSION["pre_name"],
+    "user_name" => $_SESSION["user_name"],
+    "user_lastname" => $_SESSION["user_lastname"],
+    "img" => $_SESSION["img"],
+
+);
+
+$template->assign_vars($data);
+$template->pparse('body');
---
+++ N1/model/N1_101_Model.php
@@ -0,0 +1,128 @@
+<?php
+class N1_101_Model
+{
+
+    // database connection and table name
+    private $conn;
+    
+    // object properties
+    public $activities_createdate;
+    public $activities_location;
+    public $activities_enddate;
+    public $activities_status;
+    public $activities_aspect;
+    public $activities_detill;
+    public $activities_total;
+    public $activities_name;
+    public $activities_timeday;
+    public $activities_timenint;
+    public $activities_year;
+    public $activities_trem;
+    public $activities_hour;
+    public $activities_join;
+    public $activities_max;
+    public $id;
+
+    // table 2
+    public $jo_activties2;
+    public $jo_userid;
+    public $jo_status;
+    public $jo_crdate;
+    public $jo_trem;
+    public $jo_year;
+
+    public $req_no;
+    public $dept_name;
+    public $emp_name;
+    public $sdate;
+    public $edate;
+    public $req_status_list;
+    
+    // constructor with $db as database connection
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // get table
+    public function getAll()
+    {
+        $query = "SELECT 
+                    ac.*,
+                    jo.jo_statusadmin AS adminstatus, jo.jo_status AS status
+                    FROM tb_activities AS ac  
+                    LEFT JOIN tb_joinactivity AS jo ON ac.id = jo.jo_activties
+                    WHERE  jo.jo_userid = :id AND  jo.jo_statusadmin = '0'
+                    ORDER BY  ac.activities_enddate DESC ";
+// 0 เปิด , 1 ปิด
+
+        $stmt = $this->conn->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        $stmt->bindParam(":id", $this->id);
+        try {
+            $stmt->execute();
+            return $stmt;
+        } catch (PDOException $ex) {
+            die($ex->getMessage());
+        }
+    }
+
+
+    public function search()
+    {
+        $query = "SELECT
+                    ac.activities_name AS activities_name,
+                    ac.activities_enddate AS activities_enddate,
+                    ac.activities_year AS activities_year,
+                    ac.activities_trem AS activities_trem,
+                    jo.jo_statusadmin AS adminstatus,
+                    ac.activities_join AS activities_join,
+                    ac.activities_total AS activities_total,
+                    ac.activities_hour  AS activities_hour,
+                    ac.activities_enddate AS activities_enddate,
+                    jo.jo_status AS status
+                    FROM tb_activities AS ac  
+                    LEFT JOIN tb_joinactivity AS jo ON ac.id = jo.jo_activties
+                    WHERE 1=1 ";
+
+        if (isset($this->req_no)) {
+            $query .= " AND ac.activities_name like '%" . $this->req_no . "%' ";
+        }
+
+        if (isset($this->id)) {
+            $query .= " AND  jo.jo_userid  = " . $this->id;
+        }
+
+        if (isset($this->emp_name)) {
+            $query .= " AND ac.activities_year like '%" . $this->emp_name . "%' ";
+        }
+
+        if (isset($this->dept_name)) {
+            $query .= " AND ac.activities_trem like '%" . $this->dept_name . "%' ";
+        }
+
+        if (!empty($this->sdate) && !empty($this->edate)) {
+            $query .= " AND ac.activities_enddate between '" . $this->sdate . "' AND '" . $this->edate . "'";
+        }
+
+        if (isset($this->req_status_list)) {
+            $array = str_repeat('?,', count($this->req_status_list) - 1) . '?';
+            $query .= " AND  jo.jo_statusadmin IN (" . $array . ") ";
+        }
+
+        $query .= " ORDER BY ac.activities_enddate DESC";
+        $stmt = $this->conn->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+    
+        try {
+            if (isset($this->req_status_list)) {
+                $stmt->execute($this->req_status_list);
+            } else {
+                $stmt->execute();
+            }
+
+            return $stmt;
+        } catch (PDOException $ex) {
+            die($ex->getMessage());
+        }
+    }
+// end class
+}
